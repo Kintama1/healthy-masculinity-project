008949560147ef32d9abbebc87a97f830486f3e2
Yassine Khayati <yassinkhayati20@gmail.com>
2025-05-17 14:50:04 -0400

    just changed logo

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/iconhe.svg" sizes="any" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body
